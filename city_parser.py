e05e62a7194b6420b91b1b6625a53e9fc593659b
Kevin Zhang <kevinzhang19870314@gmail.com>
2023-08-25 11:42:57 +0800

    Update city_parser.py

--- city_parser.py
+++ city_parser.py
@@ -8,6 +8,8 @@ from langchain.prompts import (
 )
 from pydantic import BaseModel, Field
 
+from log import log
+
 load_dotenv()
 
 model_name = "text-davinci-003"
@@ -16,7 +18,6 @@ model = OpenAI(model_name=model_name, temperature=temperature)
 
 
 class WeatherRequest(BaseModel):
-	setup: str = Field(description="user question")
 	city: str = Field(description="name of the city to get weather information, be sure to translate it to English")
 
 
@@ -28,13 +29,14 @@ def get_city(user_query: str):
 		partial_variables={"format_instructions": parser.get_format_instructions()},
 	)
 
+	print('\n')
+	log(f"User question: {user_query}")
 	_input = prompt.format_prompt(query=user_query)
 	try:
 		output = model(_input.to_string())
 		response: WeatherRequest = parser.parse(output)
 
-		print(f"setup = {response.setup}")
-		print(f"city = {response.city}")
+		log(f"city = {response.city}")
 
 		return response.city
 	except Exception as e:
